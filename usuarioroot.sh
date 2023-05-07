080d3342c55e650f9b7e44608eb838a170ea0238
Camelokun <107444434+Camelokun@users.noreply.github.com>
2023-05-07 14:02:44 -0500

    Update usuarioroot.sh

--- usuarioroot.sh
+++ usuarioroot.sh
@@ -1,9 +1,44 @@
 #!/bin/bash
 
-echo "Ingrese el nombre del nuevo usuario:"
-read username
+# Verificar si el usuario tiene permisos de sudo
+if [[ $(sudo -n true 2>&1) ]]; then
+    echo "Este script debe ejecutarse con permisos de sudo."
+    exit 1
+fi
 
-adduser $username
+# Verificar si la configuración de ssh ya existe antes de modificarla
+if grep -q 'prohibir-contraseña' /etc/ssh/sshd_config; then
+    sudo sed -i 's/prohibir-contraseña/sí/g' /etc/ssh/sshd_config
+fi
 
-echo "Ingrese la nueva contraseña para el usuario:"
-passwd $username
+if grep -q 'sin-contraseña' /etc/ssh/sshd_config; then
+    sudo sed -i 's/sin-contraseña/sí/g' /etc/ssh/sshd_config
+fi
+
+if grep -q '#PermitRootLogin' /etc/ssh/sshd_config; then
+    sudo sed -i 's/#PermitRootLogin/PermitRootLogin/g' /etc/ssh/sshd_config
+fi
+
+if ! grep -q 'Autenticación de contraseña' /etc/ssh/sshd_config; then
+    echo 'Autenticación de contraseña sí' | sudo tee -a /etc/ssh/sshd_config > /dev/null
+fi
+
+if grep -q 'PasswordAuthentication no' /etc/ssh/sshd_config || grep -q '#PasswordAuthentication no' /etc/ssh/sshd_config; then
+    sudo sed -i 's/#\{0,1\}PasswordAuthentication no/PasswordAuthentication sí/g' /etc/ssh/sshd_config
+fi
+
+# Reiniciar el servicio ssh
+sudo service ssh restart
+
+# Configurar el firewall
+sudo iptables -F
+sudo iptables -A INPUT -p tcp --dport 81 -j ACCEPT
+sudo iptables -A INPUT -p tcp --dport 80 -j ACCEPT
+sudo iptables -A INPUT -p tcp --dport 22 -j ACCEPT
+sudo iptables -A INPUT -p tcp -m state --state RELATED,ESTABLISHED -j ACCEPT
+sudo iptables -P INPUT DROP
+
+# Guardar la configuración del firewall
+sudo apt-get install iptables-persistent
+sudo netfilter-persistent save
+sudo netfilter-persistent reload
