cecea0432a8169a566724bfbcb6e0a340f618ac9
Camelokun <107444434+Camelokun@users.noreply.github.com>
2023-05-21 20:28:54 -0500

    Update ver_conectados.sh

--- ver_conectados.sh
+++ ver_conectados.sh
@@ -1,22 +1,28 @@
+                                           done
 #!/bin/bash
 
-echo "BIENVENIDO AL MENÚ PARA VER CONECTADOS"
-echo "Seleccione una opción:"
-echo "1. Ver conectados en el puerto 80"
-echo "2. Ver conectados en el puerto 443"
+while true; do
+  echo "BIENVENIDO AL MENÚ PARA VER CONECTADOS"
+  echo "Seleccione una opción:"
+  echo "1. Ver conectados en el puerto 80"
+  echo "2. Ver conectados en el puerto 443"
+  echo "3. Salir"
 
-read -p "Opción: " opcion
+  read -p "Opción: " opcion
 
-case $opcion in
-  1)
-    PSIPHON_PORT_80=$(sudo netstat -tn | awk '$4 ~ /:80$/ {print $5}' | cut -d: -f1 | sort | uniq -c | wc -l)
-    echo "CONEXIONES EN EL PUERTO 80 DE PSIPHON: $PSIPHON_PORT_80"
-    ;;
-  2)
-    PSIPHON_PORT_443=$(sudo netstat -tn | awk '$4 ~ /:443$/ {print $5}' | cut -d: -f1 | sort | uniq -c | wc -l)
-    echo "CONEXIONES EN EL PUERTO 443 DE PSIPHON: $PSIPHON_PORT_443"
-    ;;
-  *)
-    echo "Opción inválida"
-    ;;
-esac
+  case $opcion in
+    1)
+      PSIPHON_PORT_80=$(sudo netstat -tn | awk '$4 ~ /:80$/ {print>      echo "CONEXIONES EN EL PUERTO 80 DE PSIPHON: $PSIPHON_PORT_8>      ;;
+    2)                                                                   PSIPHON_PORT_443=$(sudo netstat -tn | awk '$4 ~ /:443$/ {pri>      echo "CONEXIONES EN EL PUERTO 443 DE PSIPHON: $PSIPHON_PORT_>      ;;
+    3)
+      echo "Saliendo del script..."
+      break
+      ;;
+    *)
+      echo "Opción inválida"
+      ;;
+  esac
+
+  echo        
+
+  done
