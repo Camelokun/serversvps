e9fe4ecc4262cab67cea5914d554a31ccac0ef04
Camelokun <107444434+Camelokun@users.noreply.github.com>
2023-07-13 15:37:56 -0500

    Add files via upload

---
+++ Onlineuser.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+while true; do
+  echo "BIENVENIDO AL MENÚ PARA VER CONECTADOS"
+  echo "Seleccione una opción:"
+  echo "1. Ver conectados en el puerto 80"
+  echo "2. Ver conectados en el puerto 443"
+  echo "3. Salir"
+
+  read -p "Opción: " opcion
+
+  case $opcion in
+  1)
+      USUARIOS ACTIVOS_PORT_443=$(sudo netstat -tn | awk '$4 ~ /:80$/ {print>      echo "USUARIOS ACTIVOS EN EL PUERTO 80: $PSIPHON_PORT_8>  ;;
+
+  2)                                                   USUARIOS ACTIVOS_80=$(sudo netstat -tn | awk '$4 ~ /:443$/ {pri>      echo "USUARIOS ACTIVOS EN EL PUERTO 80: $PSIPHON_PORT_>      ;;
+
+  3)
+      echo "Saliendo del script..."
+      break
+      ;;
+    *)
+      echo "Opción inválida"
+      ;;
+  esac
+
+  echo             
+  
+  done
