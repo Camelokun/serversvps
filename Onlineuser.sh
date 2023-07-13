bdc31ed2f23997c24c7ab01fc61646eb67863832
Camelokun <107444434+Camelokun@users.noreply.github.com>
2023-07-13 15:44:29 -0500

    Update Onlineuser.sh

--- Onlineuser.sh
+++ Onlineuser.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 while true; do
-  echo "BIENVENIDO AL MENÚ PARA VER CONECTADOS"
+  echo "SCRIPT PARA VER CONECTADOS ONLINE"
+  echo "USUARIOS ONLINE EN PUERTO 80"
+  echo "USUARIOS ONLINE EN PUERTO 443"
   echo "Seleccione una opción:"
   echo "1. Ver conectados en el puerto 80"
   echo "2. Ver conectados en el puerto 443"
@@ -10,12 +12,15 @@ while true; do
   read -p "Opción: " opcion
 
   case $opcion in
-  1)
-      USUARIOS ACTIVOS_PORT_443=$(sudo netstat -tn | awk '$4 ~ /:80$/ {print>      echo "USUARIOS ACTIVOS EN EL PUERTO 80: $PSIPHON_PORT_8>  ;;
-
-  2)                                                   USUARIOS ACTIVOS_80=$(sudo netstat -tn | awk '$4 ~ /:443$/ {pri>      echo "USUARIOS ACTIVOS EN EL PUERTO 80: $PSIPHON_PORT_>      ;;
-
-  3)
+    1)
+      PORT_80_USERS=$(sudo netstat -tn | awk '$4 ~ /:80$/ {print $5}' | cut -d: -f1 | sort | uniq -c | wc -l)
+      echo "CONEXIONES EN EL PUERTO 80: $PORT_80_USERS"
+      ;;
+    2)
+      PORT_443_USERS=$(sudo netstat -tn | awk '$4 ~ /:443$/ {print $5}' | cut -d: -f1 | sort | uniq -c | wc -l)
+      echo "CONEXIONES EN EL PUERTO 443: $PORT_443_USERS"
+      ;;
+    3)
       echo "Saliendo del script..."
       break
       ;;
@@ -24,6 +29,5 @@ while true; do
       ;;
   esac
 
-  echo             
-  
-  done
+  echo
+done
